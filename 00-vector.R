54c147added4ee1b314b90c6a092ca76e20ad52d
AbdelganiBaraka001 <baraka@Baraka.localdomain>
2023-03-23 14:04:48 +0530

    hello

---
+++ 00-vector.R
@@ -0,0 +1,28 @@
+rm(lis=ls())
+# variable typrs
+#chacter, numeric and logical
+a = 1
+b = 2
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+x = c( 1, 3, 10) 
+# sue function c() for multiple inputs
+y = c(8, 1, 100)
+# data type 
+char_vec = c('hello' 'world', ' chicken', carrot')
+num_vec = c(10, 19, 1, 100, 2, 10, 89, 8)
+# vector
+# sting split function
+split(x= 'green Apple', split = " ")
+my_names = c('R Das', 'R Babu','R Kalita')
+split(my_names[1], split = " ")[[1]][2]
+suername[1] = strsplit(my_names[1], split =" ") [[1]][2] 
+suername[2] = strsplit(my_names[2], split =" ") [[1]][2]
+suername[3] = strsplit(my_names[3], split =" ") [[1]][2 ]
+sn = c()
+for(i in 1:3) {sn [i] = strsplit (my_name 
+
