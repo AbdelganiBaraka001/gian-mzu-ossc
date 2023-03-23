c9b8515788c411016416af9635f28c86285ad628
AbdelganiBaraka001 <baraka@Baraka.localdomain>
2023-03-23 14:36:26 +0530

    you wellcome

---
+++ 01-hello-r.R
@@ -0,0 +1,19 @@
+my_var <- c(
+a= 'apple', 
+b= 'boy', 
+c= ' cat', 
+d= 'desktop'
+)
+my_var[3]
+
+my_var['c']
+
+my_var[c(3,2,1,4)]
+
+my_var[c('c', 'b', 'a', 'd')];
+
+
+
+#R function to get names of a variable
+names(my_var)
+
